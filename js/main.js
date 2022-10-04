1550476f5b81815cc03803d7b31959e42e92ac69
ArturLimaiko <alimayko91@gmail.com>
2022-10-04 22:49:35 +0300

    ф-ция проверки макс. длины строки и ф-ция возврата случайного числа

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
-function getRandomNum () {
-  let min = 1;
-  let max = 140;
-  let rand = Math.floor (min + Math.random() *  (max + 1 - min));
+/**
+ * Функция для проверки максимальной длины строки.
+ * @param {string} verifiedString Входная строка
+ * @param {number} maxLength Максимальная длина
+ * @param {boolean} Подходит ли строка по длине
+ */
 
-  console.log (rand);
+function checkMaxLengthString (verifiedString, maxLength = 56){
+  return verifiedString.length <= maxLength;
 }
-getRandomNum ();
+
+/**
+ * Возвращает случайное число в заданном диапазоне и проверяет на типы и что числа положительные
+ * @param {number} first Положительное число
+ * @param {number} second Положительное число
+ * @param {number} precision случайное число в заданном промежутке включительно
+ */
+function getRandomIntegerWithChecks(first, second) {
+  if (typeof first === 'number' || typeof second === 'number') {
+    return NaN;
+  }
+
+  if (first < 0 || second < 0) {
+    return NaN;
+  }
+
+  if (second < first) {
+    [first, second] = [second, first];
+  }
+
+  first = Math.ceil(first);
+  second = Math.floor(second);
+
+  return getRandomInteger(first, second);
+}
