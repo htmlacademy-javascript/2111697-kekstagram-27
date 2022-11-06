fd3d8fabdecc0175d029f015a402efb7f4924407
Keks <keks@htmlacademy.ru>
2022-11-06 18:00:30 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/photo-user.js
@@ -0,0 +1,27 @@
+import mockPhotos from './mock.js';
+
+/** контейнер для изображений от других пользователей */
+const imgContainer = document.querySelector('.pictures');
+
+/** шаблон миниатюр
+ * @type {HTMLAnchorElement}
+ */
+const photoTemplate = document.querySelector('#picture').content.firstElementChild;
+
+/** создаем пустой фрагмент(предварительная обёртка)
+ */
+const photoBox = document.createDocumentFragment();
+
+//на основе шаблона создаем клон который нашли выше
+for (const photo of mockPhotos) {
+  //клонируем элемент со всеми внутренностями
+  const photoElement = photoTemplate.cloneNode(true);
+
+  photoElement.querySelector('.picture__img').src = photo.url;
+  photoElement.querySelector('.picture__likes').textContent = photo.likes;
+  photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+
+  photoBox.append(photoElement);
+}
+
+imgContainer.append(photoBox);
---
+++ 7/js/upload-modal.js
@@ -0,0 +1,44 @@
+import {isEscapeKey} from './utils.js';
+import {resetScale} from './scale.js';
+
+/** @type {HTMLFormElement} */
+const uploadForm = document.querySelector('.img-upload__form');
+
+/** @type {HTMLInputElement} поле загрузки нового изображения */
+const imgUploadStart = uploadForm.querySelector('#upload-file');
+// находим форму редактирования изображения
+const editFormImage = uploadForm.querySelector('.img-upload__overlay');
+
+/**
+ * функцию закрытия на клавишу ESC
+ * @param {KeyboardEvent} evt
+ */
+const closeModalOnEscape = (evt) => {
+
+  if(isEscapeKey(evt)) {
+    evt.preventDefault();
+    const {tagName} = evt.target;
+
+    /** Если инпут или текстареа в активном состоянии, то мы не реагируем на `Esc` */
+    if (tagName !== 'INPUT' || tagName !== 'TEXTAREA') {
+      uploadForm.reset();
+    }
+  }
+};
+
+
+const toggleModalState = (toOpen = true) => () => {
+  editFormImage.classList.toggle('hidden', !toOpen);
+  document.body.classList.toggle('modal-open', toOpen);
+
+  //закрытие происходит по нажатию клавиши ESC
+  if (toOpen) {
+    document.addEventListener('keydown', closeModalOnEscape);
+  } else{
+    document.removeEventListener('keydown', closeModalOnEscape);
+  }
+  resetScale();
+};
+
+imgUploadStart.addEventListener('change', toggleModalState(true));
+uploadForm.addEventListener('reset', toggleModalState(false));
